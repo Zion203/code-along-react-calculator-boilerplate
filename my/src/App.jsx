fc82dda3643581778115ce20242223598dd1f4f3
Zion203 <jesudas.t@kalvium.community>
2024-02-03 11:46:07 +0530

    commit

---
+++ my/src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      calculateResult();
+    } else if (value === 'ac') {
+      clearAll();
+    } else if (value === 'del') {
+      deleteLast();
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  const calculateResult = () => {
+    try {
+      const calculatedResult = eval(input);
+      setResult(calculatedResult);
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const clearAll = () => {
+    setInput('');
+    setResult('');
+  };
+
+  const deleteLast = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <div className="row">
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={() => handleButtonClick('=')}>=</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('ac')}>AC</button>
+          <button onClick={() => handleButtonClick('del')}>DEL</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
